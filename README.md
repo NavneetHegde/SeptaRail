4ee80477ddcf4f38198fccef96c9399cf4ff6b1e
Navneet Hegde <navneet.hegde@freedompay.com>
2024-02-17 00:24:23 -0500

    Added GetTransFunction tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# SeptaRail - Next Three Trains
+
 [![Build Status](https://dev.azure.com/navneethegde/SeptaRail/_apis/build/status%2FNavneetHegde.SeptaRail?branchName=master)](https://dev.azure.com/navneethegde/SeptaRail/_build/latest?definitionId=11&branchName=master)
---
+++ test/GetTrainsFunction.Tests/Mocks/MockHttpRequestData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Moq;
+using System.Security.Claims;
+using System.Text;
+
+namespace GetTrainsFunction.Tests;
+
+public sealed class MockHttpRequestData : HttpRequestData
+{
+    // No behaviour is actually needed from this.
+    private static readonly FunctionContext Context = Mock.Of<FunctionContext>();
+
+    public MockHttpRequestData(string body) : base(Context)
+    {
+        if (!string.IsNullOrWhiteSpace(body))
+        {
+            var bytes = Encoding.UTF8.GetBytes(body);
+            Body = new MemoryStream(bytes);
+        }
+    }
+
+    public override HttpResponseData CreateResponse()
+    {
+        return new MockHttpResponseData(Context);
+    }
+
+    public override Stream Body { get; }
+    public override HttpHeadersCollection Headers { get; }
+    public override IReadOnlyCollection<IHttpCookie> Cookies { get; }
+    public override Uri Url { get; }
+    public override IEnumerable<ClaimsIdentity> Identities { get; }
+    public override string Method { get; }
+}
